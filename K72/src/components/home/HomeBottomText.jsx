48448b16c6dd62b30e48d2d4eb38cabf5a731b1c
Yuvrajsinghko <yuvrajprince19@gmail.com>
2025-11-15 16:22:25 +0530

    updated multiple files

--- K72/src/components/home/HomeBottomText.jsx
+++ K72/src/components/home/HomeBottomText.jsx
@@ -3,9 +3,23 @@ import { Link } from "react-router-dom";
 
 function HomeBottomText() {
   return (
-    <div className="font-[font2] flex justify-center gap-5">
-      <Link  className="text-[6.5vw] leading-[5vw] border-5 border-white rounded-full px-10 pt-6  uppercase" to='/projects'>Projects</Link>
-      <Link  className="text-[6.5vw] leading-[5vw] border-5 border-white rounded-full px-10 pt-6  uppercase" to='/agence'>Agence</Link>
+    <div className="font-[font2] flex justify-center gap-2">
+      <div>
+        <Link
+          className="text-[6.5vw] leading-[5vw] border-5 border-white rounded-full px-10 pt-6  uppercase"
+          to="/projects"
+        >
+          Projects
+        </Link>
+      </div>
+      <div>
+        <Link
+          className="text-[6.5vw] leading-[5vw] border-5 border-white rounded-full px-10 pt-6  uppercase"
+          to="/agence"
+        >
+          Agence
+        </Link>
+      </div>
     </div>
   );
 }
