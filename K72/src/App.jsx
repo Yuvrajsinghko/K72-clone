66b5d1066cb5437c61d4a184880ecf9cdd7d3c53
Yuvrajsinghko <yuvrajprince19@gmail.com>
2025-11-27 19:17:06 +0530

    mdified agence.jsx

--- K72/src/App.jsx
+++ K72/src/App.jsx
@@ -1,35 +1,47 @@
-import React from 'react'
-import { Link, Route, Routes } from 'react-router-dom'
-import Home from './pages/Home'
-import Projects from './pages/Projects'
-import Agence from './pages/Agence'
-import { useGSAP } from '@gsap/react'
-
-
+import React from "react";
+import { Link, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import Agence from "./pages/Agence";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 
 const App = () => {
+  const tl = gsap.timeline();
 
-  useGSAP(()=>{
-    
-  })
+  useGSAP(() => {
+    tl.from(".stair", {
+      height: 0,
+      stagger: {
+        amount: -0.25,
+      },
+    });
+    tl.to(".stair",{
+      y:'100%',
+      stagger: {
+        amount: -0.25,
+      },
+    })
+  });
 
   return (
-    <div className='text-white'>     
-      <div className='h-screen fixed z-10 flex w-full'>
-        <div className='h-full w-1/5 bg-amber-700'></div>
-        <div className='h-full w-1/5 bg-amber-200'></div>
-        <div className='h-full w-1/5 bg-amber-700'></div>
-        <div className='h-full w-1/5 bg-amber-200'></div>
-        <div className='h-full w-1/5 bg-amber-700'></div>
+    <div className="text-white">
+      <div className="h-screen fixed z-10 flex w-full">
+        <div className="stair h-full w-1/6 bg-black"></div>
+        <div className="stair h-full w-1/6 bg-black"></div>
+        <div className="stair h-full w-1/6 bg-black"></div>
+        <div className="stair h-full w-1/6 bg-black"></div>
+        <div className="stair h-full w-1/6 bg-black"></div>
+        <div className="stair h-full w-1/6 bg-black"></div>       
       </div>
-      
+
       <Routes>
-        <Route  path='/' element={<Home />}/>
-        <Route  path='/agence' element={<Agence />}/>
-        <Route  path='/projects' element={<Projects />}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/agence" element={<Agence />} />
+        <Route path="/projects" element={<Projects />} />
       </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
