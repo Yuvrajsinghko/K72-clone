7f6268c36f52d2e5bb8cfd1faa2a7101a55207a2
Yuvrajsinghko <yuvrajprince19@gmail.com>
2025-11-27 18:57:19 +0530

    mdified agence.jsx

--- K72/src/App.jsx
+++ K72/src/App.jsx
@@ -9,7 +9,11 @@ import Agence from './pages/Agence'
 const App = () => {
   return (
     <div className='text-white'>     
-      
+      <div className='h-screen fixed z-10 flex w-full'>
+        <div className='h-full w-1/5 bg-amber-700'></div>
+        <div className='h-full w-1/5 bg-amber-700'></div>
+        <div className='h-full w-1/5 bg-amber-700'></div>
+      </div>
       
       <Routes>
         <Route  path='/' element={<Home />}/>
